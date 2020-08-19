3f032f96a046b27548fd7d0e602b7563f302795b
Neblanck1024 <pierlca24@outlook.com>
2020-08-19 13:09:55 -0500

    Servicios, Constantes, HTTP y animaciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
 
 const app_routes: Routes = [
-    { path: 'home', component: PortafolioComponent },  
+    { path: 'home', component: PortafolioComponent },
     { path: 'about', component: AboutComponent },
     { path: 'item', component: ItemComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
@@ -15,7 +15,7 @@ const app_routes: Routes = [
 @NgModule({
     imports: [
       RouterModule.forRoot( app_routes, { useHash: true} )
-    ],exports: [
+    ], exports: [
       RouterModule
     ]
 })
---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,15 @@
+
+
+
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nombre?: string;
+  pagina_autor?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  tublr?: string;
+  equipo_trabajo?: any[];
+}
+
